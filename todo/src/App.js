44d0ff435a901ef3fbc8cb9232eae4b59b568ba4
zamindar-786 <zamindar900@gmail.com>
2021-05-24 00:15:28 +0530

    Done

---
+++ todo/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import TodoList from "./components/TodoList"
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+      
+      
+    </div>
+  )
+}
+
+export default App
+
